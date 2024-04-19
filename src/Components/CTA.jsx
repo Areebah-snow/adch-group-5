175173230e9246f9eb7b6c49a82bcc0c8daaabf6
solomonadzape95 <adzapesolomon@gmail.com>
2024-04-19 11:27:19 +0100

    Merge Conflicts Resolved

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -2,7 +2,7 @@ import { Button } from "./Hero";
 export default function CTA() {
   return (
     <section className="cta flex lg:p-36 p-6 justify-around md:flex-nowrap flex-wrap items-center">
-      <div className="flex flex-col justify-start items-center text-center lg:text-left lg:w-[70ch] w-3\4 md:gap-6 gap-8">
+      <div className="flex flex-col justify-start items-center text-center md:text-left lg:w-[70ch] w-3\4 md:gap-6 gap-8">
         <h2 className="md:text-4xl text-3xl font-semibold md:leading-10 ">
           Ready to simplify your event management process?
         </h2>
