09e9016763848ec767e351a3d8aae4fe07b7818c
Solomon Adzape <adzapesolomon@gmail.com>
2024-04-30 13:43:27 +0100

    Edited Landing Page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import png1 from "../assets/pngwing 2.png";
 const Hero = () => {
   return (
     <section>
-      <div className="max-w-[1440px] mx-auto text-center mt-2 md:mt-12 px-4">
+      <div className="max-w-[1440px] mx-auto text-center mt-2 md:mt-12 px-4 z-10">
         <div className="leading-[43.5px] md:leading-[65px] mx-auto text-[#223134] text-[32px] md:text-[48px] font-bold">
           <h1>
             The Simplest Way to <br /> Manage Your <br />
@@ -15,10 +15,10 @@ const Hero = () => {
         </p>
         <Button text={"Create Event"} />
       </div>
-      <div className="hero1 relative mt-24 px-4">
+      <div className="hero1 relative mt-24 px-4 bg-center bg-cover">
         <div className="rounded-[30px] mx-auto max-w-[1188px] h-[292px] md:h-[400px] bg-[#473BF0]"></div>
         <img
-          className="max-md:w-[350px] max-md:h-[460px] absolute bottom-4 md:bottom-0 left-1/2 transform -translate-x-1/2"
+          className="max-md:w-[350px] max-md:h-[350px] md:max-h-[500px] md:max-w-[900px] absolute bottom-4 md:bottom-0 left-1/2 transform -translate-x-1/2 z-0"
           src={png1}
           alt=""
           style={{ zIndex: 0 }}
@@ -31,7 +31,7 @@ const Hero = () => {
 // eslint-disable-next-line react/prop-types
 export function Button({ text }) {
   return (
-    <Link to="/createevent">
+    <Link to="/register">
       <button className="text-white rounded-[42px] bg-[#473BF0] text-bold text-[20px] w-[201px] h-[66px]">
         {text}
       </button>
